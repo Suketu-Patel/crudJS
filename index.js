af1ac1c548e4aa11fb776eeb7eedf915d7c0ee8c
Suketu Patel <suketu.patel@SuketuPatel.local>
2020-02-06 14:44:00 +0530

    removed switch case from update and made it simple

--- index.js
+++ index.js
@@ -1,48 +1,105 @@
 console.clear()
+console.log("=========================================================================================================================================")
 let data = [
     { name: 'Suketu Patel', email: 'suketupatel29@gmail.com', age: '21', gender: 'Male' },
     { name: 'Ashish Fatnani', email: 'ashish@gmail.com', age: '21', gender: 'Male' },
 ]
-const inputValidation = (name, email, age, gender) => {
-    //To check whether the entered username is already present
-    if (data.every((item) => (item.email === email))) {
-        console.error("Email already exists")
-        return false;
+
+const VALIDATE = (() => {
+    return {
+        name: (name) => {
+            if (/^[a-zA-Z]+(([a-zA-Z ])?[a-zA-Z]*)*$/.test(name)) {
+                return true;
+            } else {
+                console.error("Name is not valid")
+                return false
+            }
+        },
+        email: (email) => {
+            email = email.toLowerCase()
+            if (data.every((item) => (item.email !== email))) {
+                if ((/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(email)) {
+                    return true
+                } else {
+                    console.error("E-mail is not valid")
+                    return false
+                }
+            } else {
+                console.error("E-mail already exists")
+            }
+        },
+        age: (age) => {
+            if (age < 100 && age >= 18) {
+                return true
+            } else {
+                console.error("Invalid age input")
+                return false
+            }
+        },
+        gender: (gender) => {
+            gender=gender.toLowerCase();
+            if (gender === "male" || gender === "female") {
+                return true
+            } else {
+                console.error("Invalid gender input")
+                return false
+            }
+        },
+
+    }
+})();
+
+const inputValidator = (userInfo) => {
+    const { name, email, age, gender } = userInfo;
+    if (VALIDATE.name(name)
+        && VALIDATE.email(email.toLowerCase())
+        && VALIDATE.age(age)
+        && VALIDATE.gender(gender.toLowerCase())) {
+
+        return true;
+
     } else {
-        if (
-            (/^[a-zA-Z]+(([a-zA-Z ])?[a-zA-Z]*)*$/.test(name)   //Checks if the given name is valid
-                &&
-                (age < 100 && age >= 18)                        //checks if the age is withing the range
-                && 
-                (gender === "male" || gender === "female")      //checks if the input is none other then male or female
-                &&
-                ((/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(email)) //checks if it is a valid email
-            )
-        ) {
-            return true
+        return false;
+    }
+}
+const CRUD = (() => {
+    return {
+        create: (name, email, age, gender) => {
+            if (inputValidator({ name, email, age, gender })) {
+                data.push({
+                    name: name,
+                    email: email.toLowerCase(),
+                    age: age,
+                    gender: gender
+                })
+            } else {
+                console.error("Cannot create new entry due to input error(s)")
+            }
+        },
+        read: (email) => {
+            let uNode = data.filter((item) => item.email === email)
+            console.log(uNode)
+        },
+        update: (email, key, value) => {
+            if(VALIDATE[key](value)){
+                let uNode = data.filter((item) => item.email === email)
+                uNode[0][key] = value;
+            }else{
+                console.error(`${key} couldn't be updated`)
+            }
+        },
+        delete: (email)=>{
+            let newData = data.filter((item) => item.email !== email)
+            data=newData
         }
-        return false;
     }
-
-}
-function create(name, email, age, gender) {
-    if (inputValidation(name, email.toLowerCase(), age, gender.toLowerCase())) {
-        data.push({ name: name, email: email.toLowerCase(), age: age, gender: gender })
-    } else {
-        console.error("Cannot create new entry due to input error")
-    }
-}
-function update(key, value) {
-    data[key] = value;
-}
-
-function read() {
-    return data;
-}
-
-function deleter(key) {
-    console.log(`${key}: ${data[key]} deleted`)
-    delete data[key]
-}
-create("Harsh Umaretiya ", "akjsgd@gmail.com", "21", "Male")
-console.log(read())
+})();
+// CRUD.create("Suketu", "asd", "21", "Male");
+// CRUD.create("Suketu", "asd@gmail.com", "21", "Male");
+// CRUD.create("Suketu", "asd@gmsail.com", "221", "Male");
+CRUD.update("suketupatel29@gmail.com", "email", "suketupatel291@gmail.com")
+CRUD.update("suketupatel291@gmail.com", "name", "SuketuD Patel")
+CRUD.update("suketupatel291@gmail.com", "age", "22")
+console.log(data)
+CRUD.delete("suketupatel291@gmail.com")
+console.log("After Delete: ",data)
